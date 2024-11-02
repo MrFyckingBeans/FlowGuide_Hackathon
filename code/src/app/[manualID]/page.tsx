8091671cb2443364e6e1be51ea274c45999470e5
Noel Fischer <noel71475@gmail.com>
2024-11-02 02:10:04 +0100

    Get data from database

--- code/src/app/[manualID]/page.tsx
+++ code/src/app/[manualID]/page.tsx
@@ -1,11 +1,39 @@
+import { PrismaClient } from "@prisma/client";
 import UserGuide from "./UserGuide";
 
-export default function ManualView({ params }: { params: { manualID: string } }) {
+export default async function ManualView({ params }: { params: { manualID: string } }) {
+  const prisma = new PrismaClient()
+
+
+  const manual = await prisma.manual.findUnique({
+    where: {
+      id: parseInt(params.manualID),
+    },
+    include: {
+      steps: {
+        include: {
+          image: true, 
+        },
+      },
+    },
+  });
+  
+  const stepsArray = manual.steps.map((step:any) => {
+    return {
+      description: step.description,
+      imageUrl: step.image ? step.image.image_url : null,
+    };
+  });
+
+  if (!manual) {
+    return <div>Manual not found</div>
+  }
+
 
   return (
     <div>
       <h1>Manual ID: {params.manualID}</h1>
-      <UserGuide />
+      <UserGuide steps={stepsArray}/>
     </div>
   );
 }
